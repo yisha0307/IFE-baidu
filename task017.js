750d799bbf6abfd46cae1bf75b7c65b618709397
Yisha Chen <chenchen9037@126.com>
2016-11-05 22:04:53 +0800

    js写了一半，还没有加选择日期和城市的效果，明天继续写

---
+++ task017.js
@@ -0,0 +1,144 @@
+$(document).ready(function(){
+var selectcity = $("#city-select option:selected").text();
+var chartData = aqiSourceData[selectcity];
+console.log(chartData);
+console.log(typeof chartData["2016-01-01"]);
+renderChart();
+
+//渲染图标
+function renderChart() {
+ for(var i in chartData){
+$(".aqi-chart-wrap").append("<div id='"+i+"th'></div>");
+var target = $(".aqi-chart-wrap #"+i+"th");
+target.attr("title",i+","+chartData[i]);
+target.addClass("dailyformat");
+target.css({"background-color":colors[Math.floor(Math.random() * colors.length)],
+"height":chartData[i]+"px","margin-top":(yheight-chartData[i])+"px"});
+}
+}
+
+
+
+})
+
+
+var xwidth= 1260, yheight=550, 
+colors=['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+
+/* 数据格式演示
+var aqiSourceData = {
+  "北京": {
+    "2016-01-01": 10,
+    "2016-01-02": 10,
+    "2016-01-03": 10,
+    "2016-01-04": 10
+  }
+};
+*/
+
+// 以下两个函数用于随机模拟生成测试数据
+//这个函数用来输入日期模式
+function getDateStr(dat) {
+  var y = dat.getFullYear();
+  var m = dat.getMonth() + 1;
+  m = m < 10 ? '0' + m : m;
+  var d = dat.getDate();
+  d = d < 10 ? '0' + d : d;
+  return y + '-' + m + '-' + d;
+}
+//
+function randomBuildData(seed) {
+  var returnData = {};
+  var dat = new Date("2016-01-01");
+  var datStr = ''
+  for (var i = 1; i < 92; i++) {
+    datStr = getDateStr(dat);
+    returnData[datStr] = Math.ceil(Math.random() * seed); //生成一行数据：“"2016-01-01": 10”
+    dat.setDate(dat.getDate() + 1); //循环，92个数字
+  }
+  return returnData; //一个包括92个属性的对象(每个属性是一个日期)
+}
+
+var aqiSourceData = {
+  "北京": randomBuildData(500),
+  "上海": randomBuildData(300),
+  "广州": randomBuildData(200),
+  "深圳": randomBuildData(100),
+  "成都": randomBuildData(300),
+  "西安": randomBuildData(500),
+  "福州": randomBuildData(100),
+  "厦门": randomBuildData(100),
+  "沈阳": randomBuildData(500)
+};
+
+// 用于渲染图表的数据
+var chartData = {};
+
+// 记录当前页面的表单选项
+var pageState = {
+  nowSelectCity: -1,
+  nowGraTime: "day"
+}
+
+/**
+ * 渲染图表
+ */
+
+
+/**
+ * 日、周、月的radio事件点击时的处理函数
+ */
+function graTimeChange() {
+  // 确定是否选项发生了变化 
+
+  // 设置对应数据
+
+  // 调用图表渲染函数
+}
+
+/**
+ * select发生变化时的处理函数
+ */
+function citySelectChange() {
+  // 确定是否选项发生了变化 
+
+  // 设置对应数据
+
+  // 调用图表渲染函数
+}
+
+/**
+ * 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
+ */
+function initGraTimeForm() {
+
+}
+
+/**
+ * 初始化城市Select下拉选择框中的选项
+ */
+function initCitySelector() {
+  // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
+
+  // 给select设置事件，当选项发生变化时调用函数citySelectChange
+
+}
+
+/**
+ * 初始化图表需要的数据格式
+ */
+function initAqiChartData() {
+  // 将原始的源数据处理成图表需要的数据格式
+  // 处理好的数据存到 chartData 中
+}
+
+/**
+ * 初始化函数
+ */
+function init() {
+  initGraTimeForm()
+  initCitySelector();
+  initAqiChartData();
+}
+
+init();
