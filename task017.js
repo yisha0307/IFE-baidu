bbe6ff0386d16dfe7165763cda509b94e6160bbc
Yisha Chen <chenchen9037@126.com>
2016-11-06 15:03:08 +0800

    改完啦！写了两个函数，可以将每天的数据通过平均数处理成周数据和月数据

--- task017.js
+++ task017.js
@@ -1,17 +1,80 @@
 $(document).ready(function(){
-$("#city-select").change(function(){var selectcity = $("#city-select option:selected").text();
-var chartData = aqiSourceData[selectcity];
-renderChart();
+$("#city-select, #form-gra-time").change(function(){var selectcity = $("#city-select option:selected").text(),chartData={},
+  chartDataDay = aqiSourceData[selectcity],name = $("#form-gra-time input:checked").val();
+
+// 数据格式
+// chartData={
+//   day:{ "2016-01-01": 10,
+//     "2016-01-02": 10,
+//     "2016-01-03": 10,
+//     "2016-01-04": 10},
+//   week:{"2016-01-第一周":10,
+//     "2016-01-第二周":10,
+//     "2016-01-第三周":10}，
+//   moonth:{
+//     "2016-01":100,
+//     "2016-02":200
+//   }
+// }
+
+
+//处理下数据（week)
+var chartDataWeek={};
+var arrobj=[];
+for(var i in chartDataDay){arrobj.push(chartDataDay[i]);};
+
+function hander(arr){
+  var arrr=[];
+  for(var i=0;i<arr.length;i+=7){
+    var temp=[];
+    for(var j=i;j<i+7;j++){
+      if(arr[j]){temp.push(arr[j]);}
+    }
+    var num=temp.reduce(function(a,b){return(a+b);});
+    arrr.push(Math.round(num=num/7));}
+    return arrr;}
+
+var arrweek = hander(arrobj);
+for(var j=0;j<arrweek.length;j++){
+  chartDataWeek["2016-第"+(j+1)+"周"] = arrweek[j];
+}
+
+//处理下数据（month）
+var chartDataMonth={};
+
+function hander2(arr){
+  var arrr=[];
+  for(var i=0;i<arr.length;i+=31){
+    var temp=[];
+    for(var j=i;j<i+31;j++){
+      if(arr[j]){temp.push(arr[j]);}
+    }
+    var num=temp.reduce(function(a,b){return(a+b);});
+    arrr.push(Math.round(num=num/31));}
+    return arrr;}
+  
+  var arrmonth = hander2(arrobj);
+  for(var m=0;m<arrmonth.length;m++){
+  chartDataMonth["2016-0"+(m+1)] = arrmonth[m];
+}
+
+chartData["day"]=chartDataDay;
+chartData["week"]=chartDataWeek;
+chartData["month"]=chartDataMonth;
+
 
 //渲染图标
+$(".aqi-chart-wrap").empty();
+renderChart();
 function renderChart() {
- for(var i in chartData){
+ for(var i in chartData[name]){
 $(".aqi-chart-wrap").append("<div id='"+i+"th'></div>");
 var target = $(".aqi-chart-wrap #"+i+"th");
-target.attr("title",i+","+chartData[i]);
-target.addClass("dailyformat");
+target.attr("title",i+", "+chartData[name][i]);
+target.removeClass();
+target.addClass(name+"format");
 target.css({"background-color":colors[Math.floor(Math.random() * colors.length)],
-"height":chartData[i]+"px","margin-top":(yheight-chartData[i])+"px"});
+"height":chartData[name][i]+"px","margin-top":(yheight-chartData[name][i])+"px"});
 }
 }})
 
@@ -68,74 +131,3 @@ var aqiSourceData = {
   "沈阳": randomBuildData(500)
 };
 
-// 用于渲染图表的数据
-var chartData = {};
-
-// 记录当前页面的表单选项
-var pageState = {
-  nowSelectCity: -1,
-  nowGraTime: "day"
-}
-
-/**
- * 渲染图表
- */
-
-
-/**
- * 日、周、月的radio事件点击时的处理函数
- */
-function graTimeChange() {
-  // 确定是否选项发生了变化 
-
-  // 设置对应数据
-
-  // 调用图表渲染函数
-}
-
-/**
- * select发生变化时的处理函数
- */
-function citySelectChange() {
-  // 确定是否选项发生了变化 
-
-  // 设置对应数据
-
-  // 调用图表渲染函数
-}
-
-/**
- * 初始化日、周、月的radio事件，当点击时，调用函数graTimeChange
- */
-function initGraTimeForm() {
-
-}
-
-/**
- * 初始化城市Select下拉选择框中的选项
- */
-function initCitySelector() {
-  // 读取aqiSourceData中的城市，然后设置id为city-select的下拉列表中的选项
-
-  // 给select设置事件，当选项发生变化时调用函数citySelectChange
-
-}
-
-/**
- * 初始化图表需要的数据格式
- */
-function initAqiChartData() {
-  // 将原始的源数据处理成图表需要的数据格式
-  // 处理好的数据存到 chartData 中
-}
-
-/**
- * 初始化函数
- */
-function init() {
-  initGraTimeForm()
-  initCitySelector();
-  initAqiChartData();
-}
-
-init();
