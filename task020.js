a9f6c25b3587f6cb1471ffe806801b50cd2440e1
Yisha Chen <chenchen9037@126.com>
2016-11-11 16:56:10 +0800

    主要是regExp
    var exp =/[^0-9a-zA-Z\u4e00-\u9fa5]+/ 这个是match“非”数字、中英文的符号

---
+++ task020.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$("#insert").click(function(){
+		var exp =/[^0-9a-zA-Z\u4e00-\u9fa5]+/; //取反操作，这个式子的意思是非中英文及数字
+		var textStr = $('#textarea').val().trim();
+		textStr = textStr.split(exp).join(' ');
+		$('#showplace').text(textStr);
+		console.log(textStr);
+	})
+	$('#search').click(function(){
+		var temp = $('#text').val().trim();
+		var textStr = $('#showplace').text();
+		if(temp != null && temp.length >0){
+			//用replace直接换textStr中的temp
+				textStr = textStr.replace(new RegExp(temp,'g'), '<span class="matched">'+temp+'</span>');
+			}
+			$('#showplace').html('<div>'+ textStr+ '</div');
+	});
+
+})
