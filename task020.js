c3abaced3590e84d22ad32da8c2a178290abed9b
Yisha Chen <chenchen9037@126.com>
2016-11-11 16:59:38 +0800

    Update task020.js

--- task020.js
+++ task020.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$("#insert").click(function(){
 		var exp =/[^0-9a-zA-Z\u4e00-\u9fa5]+/; //取反操作，这个式子的意思是非中英文及数字
-		var textStr = $('#textarea').val().trim();
+		var textStr = $('#textarea').val().trim();//textarea和input一样是value，而不是text
 		textStr = textStr.split(exp).join(' ');
 		$('#showplace').text(textStr);
 		console.log(textStr);
